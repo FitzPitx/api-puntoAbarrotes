da7c27329610f867ada8aa4083c288d4a3cd5639
FitzPitx <anieto@uniempresarial.edu.co>
2023-09-27 12:46:33 -0500

    add eliminate

--- loginUser.php
+++ loginUser.php
@@ -5,12 +5,12 @@ require('connection.php');
 $user_contra=md5($_POST['user_contra']);
 $user_mail=$_POST['user_mail'];
 
-$checkUser = "SELECT * FROM userapi WHERE user_mail = '$user_mail'";
+$checkUser = "SELECT * FROM userapi WHERE user_mail = '$user_mail' AND user_status = '1'";
 $result = mysqli_query($con, $checkUser);
 
 if(mysqli_num_rows($result) > 0){
 
-    $checkUserQuery = "SELECT user_id, user_mail, user_usuario FROM userapi WHERE user_mail = '$user_mail' AND user_contra = '$user_contra'";
+    $checkUserQuery = "SELECT user_id, user_nombre, user_apellido, user_mail, user_usuario FROM userapi WHERE user_mail = '$user_mail' AND user_contra = '$user_contra'";
     $resultant = mysqli_query($con, $checkUserQuery);
 
     if(mysqli_num_rows($resultant) > 0){
@@ -27,7 +27,7 @@ if(mysqli_num_rows($result) > 0){
 } else {
     $response['Usuarios'] = (object)[];
     $response['error'] = "400";
-    $response['message'] = "Usuario no registrado";
+    $response['message'] = "Usuario no registrado o inactivo";
     
 }
 
